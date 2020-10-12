15626d5ecab0c124cb8d3cf936abc8af7f9b2589
Laurent Binder <laurentbinder@gmail.com>
2020-10-12 10:45:48 +0200

    darts

---
+++ ruby/darts/darts.rb
@@ -0,0 +1,32 @@
+=begin
+Write your code for the 'Darts' exercise in this file. Make the tests in
+`darts_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/darts` directory.
+=end
+
+class Darts
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def score
+    # (x-a)² +(y-b)² = R²
+    # Centre(0, 0)
+    # x²+y² = R²
+    target = x**2 + y**2
+    if target <= 1
+      10
+    elsif target <= 5 && target > 1
+      5
+    elsif target <= 10 && target > 5
+      1
+    else
+      0
+    end
+  end
+end
