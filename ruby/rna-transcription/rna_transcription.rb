62a1e6857f5c7f4d689c26dba0aa0d87022552bc
Laurent Binder <laurentbinder@gmail.com>
2020-10-15 17:07:00 +0200

    shorter method

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,38 +1,15 @@
-=begin
-Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
-`rna_transcription_test.rb` pass.
+# * `G` -> `C`
+# * `C` -> `G`
+# * `T` -> `A`
+# * `A` -> `U`
 
-To get started with TDD, see the `README.md` file in your
-`ruby/rna-transcription` directory.
-=end
-
-
-def rna_transcription(dna)
-  rna = []
-  # dna.!empty?
-  dna.chars.each do |value|
-    if value == 'G'
-      rna << 'C'
-    elsif value == 'C'
-      rna << 'G'
-    elsif value == 'A'
-      rna << 'T'
-    else
-      rna << 'A'
-    end
+# class Complement
+class Complement
+  def self.of_dna(sequence)
+    sequence.tr('GCTA', 'CGAU')
   end
-  rna.join
 end
 
-def sequence_rna_to_dna
-  # * `G` -> `C`
-  # * `C` -> `G`
-  # * `A` -> `T`
-  # * `U` -> `A`
-end
+# p rna_transcription('ACGT')
 
-p rna_transcription('ACGT')
-
-p rna_transcription('UGCACCAGAAUU') == 'ACGTGGTCTTAA'
-
-
+# p rna_transcription('UGCACCAGAAUU') == 'ACGTGGTCTTAA'
