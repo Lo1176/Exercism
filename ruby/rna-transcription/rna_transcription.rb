61fdeeffe9e674cc9f6104057e49a108c390eb20
Laurent Binder <laurentbinder@gmail.com>
2020-10-15 16:28:07 +0200

    rna_transcription done

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -8,14 +8,31 @@ To get started with TDD, see the `README.md` file in your
 
 
 def rna_transcription(dna)
-  dna.split('')
-  '' if dna.empty?
+  rna = []
+  # dna.!empty?
+  dna.chars.each do |value|
+    if value == 'G'
+      rna << 'C'
+    elsif value == 'C'
+      rna << 'G'
+    elsif value == 'A'
+      rna << 'T'
+    else
+      rna << 'A'
+    end
+  end
+  rna.join
 end
 
 def sequence_rna_to_dna
   # * `G` -> `C`
   # * `C` -> `G`
-  # * `T` -> `A`
-  # * `A` -> `U`
+  # * `A` -> `T`
+  # * `U` -> `A`
 end
+
+p rna_transcription('ACGT')
+
+p rna_transcription('UGCACCAGAAUU') == 'ACGTGGTCTTAA'
+
 
