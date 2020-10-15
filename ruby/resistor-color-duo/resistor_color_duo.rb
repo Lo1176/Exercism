af36d462c7729ff5fab4e69cc82c5512e8bbbed8
Laurent Binder <laurentbinder@gmail.com>
2020-10-15 12:25:51 +0200

    resistor_color_duo all pass

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,3 +6,18 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+class ResistorColorDuo
+  COLORS = %w[Black Brown Red Orange Yellow Green Blue Violet Grey White]
+  def self.value(color)
+    result = ''
+    two_color = selected_color(color)
+    two_color.each do |c|
+      result += COLORS.index(c.capitalize).to_s
+    end
+    result.to_i
+  end
+
+  def self.selected_color(color)
+    color.select { |c| color.index(c) < 2 }
+  end
+end
