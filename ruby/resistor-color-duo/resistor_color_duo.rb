2d4a640b65c66df5876aa091900f152af93738be
Laurent Binder <laurentbinder@gmail.com>
2020-10-17 17:30:48 +0200

    refoacto

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,23 +1,7 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
 class ResistorColorDuo
-  COLORS = %w[Black Brown Red Orange Yellow Green Blue Violet Grey White]
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(color)
-    result = ''
-    two_color = selected_color(color)
-    two_color.each do |c|
-      result += COLORS.index(c.capitalize).to_s
-    end
-    result.to_i
-  end
-
-  def self.selected_color(color)
-    color.select { |c| color.index(c) < 2 }
+    two_color = color.take(2)
+    two_color.map { |c| COLORS.index(c) }.join.to_i
   end
 end
