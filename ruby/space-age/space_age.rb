ed12b232a871d50c875e805d5d164e228a5ccda8
Laurent Binder <laurentbinder@gmail.com>
2020-10-23 15:41:35 +0200

    refacto with define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -6,57 +6,29 @@ To get started with TDD, see the `README.md` file in your
 `ruby/space-age` directory.
 =end
 
-EARTH = 31_557_600
-MERCURY = EARTH * 0.2408467
-VENUS = EARTH * 0.61519726
-MARS = EARTH * 1.8808158
-JUPITER = EARTH * 11.862615
-SATURN = EARTH * 29.447498
-URANUS = EARTH * 84.016846
-NEPTUNE = EARTH * 164.79132
+# EYIS = EARTH_YEAR_IN_SECONDS
+EYIS = 31_557_600
+ORBITAL_PERIODS_IN_EARTH_YEARS = {
+  earth: EYIS,
+  mercury: EYIS * 0.2408467,
+  venus: EYIS * 0.61519726,
+  mars: EYIS * 1.8808158,
+  jupiter: EYIS * 11.862615,
+  saturn: EYIS * 29.447498,
+  uranus: EYIS * 84.016846,
+  neptune: EYIS * 164.79132
+}.freeze
 
 # class SpaceAge
 class SpaceAge
-  attr_reader :age
-  def initialize(age)
-    @age = age
+  attr_reader :seconds
+  def initialize(seconds)
+    @seconds = seconds
   end
 
-  def on_earth
-    age_on_planet(EARTH)
-  end
-
-  def on_mercury
-    age_on_planet(MERCURY)
-  end
-
-  def on_venus
-    age_on_planet(VENUS)
-  end
-
-  def on_mars
-    age_on_planet(MARS)
-  end
-
-  def on_jupiter
-    age_on_planet(JUPITER)
-  end
-
-  def on_saturn
-    age_on_planet(SATURN)
-  end
-
-  def on_uranus
-    age_on_planet(URANUS)
-  end
-
-  def on_neptune
-    age_on_planet(NEPTUNE)
-  end
-
-  private
-
-  def age_on_planet(planet)
-    age.fdiv(planet).round(2)
+  ORBITAL_PERIODS_IN_EARTH_YEARS.each do |planet, periods|
+    define_method("on_#{planet}") do
+      seconds.fdiv(periods).round(2)
+    end
   end
 end
