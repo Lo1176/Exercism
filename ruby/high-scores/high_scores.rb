a008e94f1cdb27da8cfb4564ca0b7c73bf210d9f
Laurent Binder <laurentbinder@gmail.com>
2020-10-23 14:25:28 +0200

    all pass

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -6,3 +6,30 @@ To get started with TDD, see the `README.md` file in your
 `ruby/high-scores` directory.
 =end
 
+# HighScores class
+class HighScores
+  attr_accessor :scores
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def list_of_scores
+    scores
+  end
+
+  def latest
+    scores.last
+  end
+
+  def personal_best
+    scores.sort.reverse.first
+  end
+
+  def personal_top_three
+    scores.sort.last(3).reverse
+  end
+
+  def latest_is_personal_best?
+    latest >= personal_top_three[0]
+  end
+end
