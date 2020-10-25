3153a358b54b18bc14ec298bd97699e3c27d8c1f
Laurent Binder <laurentbinder@gmail.com>
2020-10-25 10:29:16 +0100

    with a hash

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,10 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+// array //
+// const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+// const colorCode = (color) => {
+//   return COLORS.indexOf(color);
+// };
+
+// ------------------ //
+// hash //
+
+const colorsWithCodes = {black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9}
 
 const colorCode = (color) => {
-  return COLORS.indexOf(color);
+  return colorsWithCodes[color]
 };
 
+const COLORS = Object.keys(colorsWithCodes);
+
+// export
 export {colorCode, COLORS}
