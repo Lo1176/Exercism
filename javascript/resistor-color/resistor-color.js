ecf67773717e81e7a6b160edcc3b59b1990f944e
Laurent Binder <laurentbinder@gmail.com>
2020-10-25 09:40:45 +0100

    toLowerCase is not necessary

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -5,7 +5,7 @@
 
 
 export const colorCode = (color) => {
-  return COLORS.indexOf(color.toLowerCase());
+  return COLORS.indexOf(color);
 };
 
 export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
