d004d062b4ad0720136f9bbc0c4f2f49c85d18b2
Laurent Binder <laurentbinder@gmail.com>
2020-10-25 10:30:02 +0100

    spaces

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -10,9 +10,11 @@
 //   return COLORS.indexOf(color);
 // };
 
-// ------------------ //
-// hash //
 
+// ------------------ //
+
+
+// hash //
 const colorsWithCodes = {black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9}
 
 const colorCode = (color) => {
