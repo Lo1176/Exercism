581db5268c004a2e4dd3c582b1270ee7a83c7b99
Laurent Binder <laurentbinder@gmail.com>
2020-11-03 18:22:02 +0100

    all pass

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,12 +2,13 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export const decodedValue = (array) => {
-  array.forEach(color => {
-    colors.indexOf(color.toLowerCase())
+  const two_colors = array.slice(0, 2);
+  const resistorColor = two_colors.map(color => {
+    return colors.indexOf(color.toLowerCase())
   });
+  return parseInt(resistorColor.join(""));
 
 };
