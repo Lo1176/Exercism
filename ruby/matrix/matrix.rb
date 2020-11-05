77c289f2a418eacb62e5dc69aa4c3501a7f43d19
Laurent Binder <laurentbinder@gmail.com>
2020-11-05 13:03:25 +0100

    4 passed

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,20 +1,21 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+# frozen_string_literal: true
 
 # class Matrix
 class Matrix
-  attr_reader :argument
+  attr_reader :matrix_of_number
 
-  def initialize(argument)
-    @argument = argument
+  def initialize(matrix_of_number)
+    @matrix_of_number = matrix_of_number
   end
 
   def rows
+    matrix_of_number
+      .split("\n")
+      .map { |string| string.split(' ') }
+      .map { |ary| ary.map(&:to_i) }
+  end
 
+  def columns
+    rows.map(&:first)
   end
 end
