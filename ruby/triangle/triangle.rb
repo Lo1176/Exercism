0627bb6821dc18a088799593324f8f0dff55b465
Laurent Binder <laurentbinder@gmail.com>
2020-11-07 10:37:10 +0100

    initialize

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -2,5 +2,10 @@
 
 # a class for triangle and methods to etermine if a triangle is equilateral, isosceles, or scalene.
 class Triangle
+  attr_reader :mesure
+
+  def initialize(mesure)
+    @mesure = mesure
+  end
 
 end
