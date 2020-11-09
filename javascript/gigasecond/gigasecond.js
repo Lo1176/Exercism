96decc6c1cc415f248c90256456ce90b69f79b85
Laurent Binder <laurentbinder@gmail.com>
2020-11-09 20:09:52 +0100

    Math

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (date) => {
+  const GIGASECOND = new Date(Math.pow(10, 8)) ;
+  return (Math.pow(date, 10) + GIGASECOND);
+
 };
